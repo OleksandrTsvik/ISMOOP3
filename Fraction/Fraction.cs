b82b66e0412e248f20b423ef36adaf2cd82f3c08
Oleksandr Tsvik <oleksandr.zwick@gmail.com>
2020-12-11 23:32:40 +0200

    ISMOOP3

---
+++ Fraction/Fraction.cs
@@ -0,0 +1,396 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FractionLibrary
+{
+    public class Fraction
+    {
+        protected long Numerator;
+        protected long Denominator;
+
+        public Fraction()
+        {
+            Numerator = 1;
+            Denominator = 1;
+        }
+
+        public Fraction(long numerator, long denominator = 1)
+        {
+            SetNumerator(numerator);
+            SetDenominator(denominator);
+        }
+
+        public Fraction(double numerator, double denominator)
+        {
+            SetNumerator(numerator);
+            SetDenominator(denominator);
+        }
+
+        public Fraction(double numerator, long denominator = 1)
+        {
+            SetNumerator(numerator);
+            SetDenominator(denominator);
+        }
+
+        public Fraction(long numerator, double denominator)
+        {
+            SetNumerator(numerator);
+            SetDenominator(denominator);
+        }
+
+        public Fraction(string fraction)
+        {
+            SetNumerator(fraction);
+            SetDenominator(fraction);
+        }
+
+        public void SetNumerator(long numerator)
+        {
+            Numerator = numerator;
+        }
+
+        public void SetDenominator(long denominator)
+        {
+            if (Numerator >= 0 && denominator < 0)
+            {
+                Numerator = -Numerator;
+                Denominator = Math.Abs(denominator);
+            }
+            else if (Numerator < 0 && denominator < 0)
+            {
+                Denominator = Math.Abs(denominator);
+                Numerator = Math.Abs(Numerator);
+            }
+            else if (denominator == 0)
+                Denominator = 1;
+            else
+                Denominator = denominator;
+        }
+
+        public void SetNumerator(double numerator)
+        {
+            Numerator = (long)Math.Round(numerator);
+        }
+
+        public void SetDenominator(double denominator)
+        {
+            SetDenominator((long)Math.Round(denominator));
+        }
+
+        public void SetNumerator(string fraction)
+        {
+            Regex regex = new Regex(@"(\-?\d+([\.\,]\d+)?)\/(\-?\d+([\.\,]\d+)?)");
+            Match match = regex.Match(fraction);
+            string num = match.Groups[1].Value.Replace('.', ',');
+
+            double numerator = 1;
+            if (num.Length > 0)
+                numerator = double.Parse(num.ToString());
+            Numerator = (long)Math.Round(numerator);
+        }
+
+        public void SetDenominator(string fraction)
+        {
+            Regex regex = new Regex(@"(\-?\d+(\.\d+)?)\/(\-?\d+(\.\d+)?)");
+            Match match = regex.Match(fraction);
+            string den = match.Groups[3].Value.Replace('.', ',');
+
+            double denominator = 1;
+            if (den.Length > 0)
+                denominator = double.Parse(den.ToString());
+
+            SetDenominator((long)Math.Round(denominator));
+        }
+
+        public long GetNumerator()
+        {
+            return Numerator;
+        }
+
+        public long GetDenominator()
+        {
+            return Denominator;
+        }
+
+        public static Fraction operator +(Fraction fraction)
+        {
+            return fraction;
+        }
+
+        public static Fraction operator -(Fraction fraction)
+        {
+            if (fraction.GetNumerator() == 0)
+                return new Fraction(fraction.GetNumerator(), fraction.GetDenominator());
+            return new Fraction(-fraction.GetNumerator(), fraction.GetDenominator());
+        }
+
+        public static Fraction operator +(Fraction fraction1, Fraction fraction2)
+        {
+            if (fraction1.GetNumerator() == 0 && fraction2.GetNumerator() == 0)
+                return new Fraction(0, NSK(fraction1.GetDenominator(), fraction2.GetDenominator()));
+            else if (fraction1.GetNumerator() == 0 && fraction2.GetNumerator() != 0)
+                return new Fraction(fraction2.GetNumerator(), fraction2.GetDenominator());
+            else if (fraction1.GetNumerator() != 0 && fraction2.GetNumerator() == 0)
+                return new Fraction(fraction1.GetNumerator(), fraction1.GetDenominator());
+
+            return new Fraction(ReductionNumerator(fraction1, fraction2) + ReductionNumerator(fraction2, fraction1), NSK(fraction1.GetDenominator(), fraction2.GetDenominator()));
+        }
+
+        public static Fraction operator +(Fraction fraction, long x)
+        {
+            return new Fraction(fraction.GetNumerator() + x * fraction.GetDenominator(), fraction.GetDenominator());
+        }
+
+        public static Fraction operator +(long x, Fraction fraction)
+        {
+            return fraction + x;
+        }
+
+        public static Fraction operator -(Fraction fraction, long x)
+        {
+            return fraction + -x;
+        }
+
+        public static Fraction operator -(long x, Fraction fraction)
+        {
+            return x + -fraction;
+        }
+
+        public static Fraction operator -(Fraction fraction1, Fraction fraction2)
+        {
+            return fraction1 + -fraction2;
+        }
+
+        public static Fraction operator *(Fraction fraction1, Fraction fraction2)
+        {
+            return new Fraction(fraction1.GetNumerator() * fraction2.GetNumerator(), fraction1.GetDenominator() * fraction2.GetDenominator());
+        }
+
+        public static Fraction operator *(Fraction fraction, long x)
+        {
+            return new Fraction(fraction.GetNumerator() * x, fraction.GetDenominator());
+        }
+
+        public static Fraction operator *(long x, Fraction fraction)
+        {
+            return fraction * x;
+        }
+
+        public static Fraction operator /(Fraction fraction1, Fraction fraction2)
+        {
+            return new Fraction(fraction1.GetNumerator() * fraction2.GetDenominator(), fraction1.GetDenominator() * fraction2.GetNumerator());
+        }
+
+        public static Fraction operator /(Fraction fraction, long x)
+        {
+            return new Fraction(fraction.GetNumerator(), fraction.GetDenominator() * x);
+        }
+
+        public static Fraction operator /(long x, Fraction fraction)
+        {
+            return new Fraction(x * fraction.GetDenominator(), fraction.GetNumerator());
+        }
+
+        public static bool operator >(Fraction fraction1, Fraction fraction2)
+        {
+            return ReductionNumerator(fraction1, fraction2) > ReductionNumerator(fraction2, fraction1);
+        }
+
+        public static bool operator <(Fraction fraction1, Fraction fraction2)
+        {
+            return ReductionNumerator(fraction1, fraction2) < ReductionNumerator(fraction2, fraction1);
+        }
+
+        public static bool operator >=(Fraction fraction1, Fraction fraction2)
+        {
+            return !(fraction1 < fraction2);
+        }
+
+        public static bool operator <=(Fraction fraction1, Fraction fraction2)
+        {
+            return !(fraction1 > fraction2);
+        }
+
+        public static bool operator ==(Fraction fraction1, Fraction fraction2)
+        {
+            return ReductionNumerator(fraction1, fraction2) == ReductionNumerator(fraction2, fraction1);
+        }
+
+        public static bool operator !=(Fraction fraction1, Fraction fraction2)
+        {
+            return !(fraction1 == fraction2);
+        }
+
+        public static explicit operator double(Fraction fraction)
+        {
+            return (double)fraction.GetNumerator() / fraction.GetDenominator();
+        }
+
+        public static double operator +(double x, Fraction fraction)
+        {
+            return x + (double)fraction;
+        }
+
+        public static double operator +(Fraction fraction, double x)
+        {
+            return (double)fraction + x;
+        }
+
+        public static double operator -(double x, Fraction fraction)
+        {
+            return x - (double)fraction;
+        }
+
+        public static double operator -(Fraction fraction, double x)
+        {
+            return (double)fraction - x;
+        }
+
+        public static double operator *(double x, Fraction fraction)
+        {
+            return x * (double)fraction;
+        }
+
+        public static double operator *(Fraction fraction, double x)
+        {
+            return (double)fraction * x;
+        }
+
+        public static double operator /(double x, Fraction fraction)
+        {
+            return x / (double)fraction;
+        }
+
+        public static double operator /(Fraction fraction, double x)
+        {
+            return (double)fraction / x;
+        }
+
+        public static bool operator >(double x, Fraction fraction)
+        {
+            return x > (double)fraction;
+        }
+
+        public static bool operator >(Fraction fraction, double x)
+        {
+            return (double)fraction > x;
+        }
+
+        public static bool operator <(double x, Fraction fraction)
+        {
+            return x < (double)fraction;
+        }
+
+        public static bool operator <(Fraction fraction, double x)
+        {
+            return (double)fraction < x;
+        }
+
+        public static bool operator >=(double x, Fraction fraction)
+        {
+            return x >= (double)fraction;
+        }
+
+        public static bool operator >=(Fraction fraction, double x)
+        {
+            return (double)fraction >= x;
+        }
+
+        public static bool operator <=(double x, Fraction fraction)
+        {
+            return x <= (double)fraction;
+        }
+
+        public static bool operator <=(Fraction fraction, double x)
+        {
+            return (double)fraction <= x;
+        }
+
+        public static bool operator ==(double x, Fraction fraction)
+        {
+            return x == (double)fraction;
+        }
+
+        public static bool operator ==(Fraction fraction, double x)
+        {
+            return (double)fraction == x;
+        }
+
+        public static bool operator !=(double x, Fraction fraction)
+        {
+            return x != (double)fraction;
+        }
+
+        public static bool operator !=(Fraction fraction, double x)
+        {
+            return (double)fraction != x;
+        }
+
+        public void FractionReduction(long numerator, long denominator)
+        {
+            if (numerator != denominator)
+            {
+                SetNumerator(numerator / NSD(numerator, denominator));
+                SetDenominator(denominator/ NSD(numerator, denominator));
+            }
+            else
+            {
+                SetNumerator(1);
+                SetDenominator(1);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Numerator}/{Denominator}";
+        }
+
+        private long NSD(long a, long b)
+        {
+            if (a == 0 || b == 0)
+                return 1;
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+            if (a == b) return a;
+            else if (a > b) return NSD(a - b, b);
+            else return NSD(a, b - a);
+        }
+
+        private static long NSK(long a, long b)
+        {
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+            long nsk = Math.Max(a, b);
+            while (nsk % a != 0 || nsk % b != 0)
+                if (a > b)
+                    nsk += a;
+                else
+                    nsk += b;
+            return nsk;
+        }
+
+        private static long ReductionNumerator(Fraction fraction1, Fraction fraction2)
+        {
+            long a = NSK(fraction1.GetDenominator(), fraction2.GetDenominator());
+            return (a / fraction1.GetDenominator()) * fraction1.GetNumerator();
+        }
+
+        public Fraction DoubleToFraction(double a)
+        {
+            Regex regex = new Regex(@"(\-?\d+)[\.\,](\d+)");
+            Match match = regex.Match(a.ToString());
+            
+            string numer = match.Groups[1].Value.ToString();
+            string denom = match.Groups[2].Value.ToString();
+
+            long numerator = long.Parse(numer + denom);
+            long denominator = (long)Math.Pow(10, denom.Length);
+
+            return new Fraction(numerator, denominator);
+        }
+    }
+}
---
+++ FractionConsoleApp/Program.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FractionLibrary;
+
+namespace FractionConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+
+            int index;
+            List<Fraction> fractions = new List<Fraction>();
+            fractions.Add(new Fraction("asd---12,0/34,asd"));
+            fractions.Add(new Fraction("3/5"));
+            fractions.Add(new Fraction("3.4/5.6"));
+            fractions.Add(new Fraction(13));
+            fractions.Add(new Fraction("4,1/8.21"));
+            fractions.Add(new Fraction(-7, -13));
+            fractions.Add(new Fraction(16.37, 7));
+            fractions.Add(new Fraction(-36, 4));
+            fractions.Add(new Fraction("a/b"));
+
+            fractions[0].SetNumerator(37);
+            Console.WriteLine($"fractions[0]: Numerator = {fractions[0].GetNumerator()} Denominator = {fractions[0].GetDenominator()}   -->   {fractions[0]}\n");
+
+            fractions[1].SetDenominator(12);
+            Console.WriteLine($"fractions[1]: Numerator = {fractions[1].GetNumerator()} Denominator = {fractions[1].GetDenominator()}   -->   {fractions[1]}\n");
+
+            fractions[2].SetNumerator("52/16");
+            fractions[2].SetDenominator("52/16");
+            Console.WriteLine($"fractions[2]: Numerator = {fractions[2].GetNumerator()} Denominator = {fractions[2].GetDenominator()}   -->   {fractions[2]}\n");
+
+            Console.WriteLine("\t### До скорочення дробів ###");
+            index = 0;
+            foreach (Fraction fraction in fractions)
+            {
+                Console.WriteLine($"fractions[{index}] = {fraction}");
+                index++;
+            }
+
+            foreach (Fraction fraction in fractions)
+                fraction.FractionReduction(fraction.GetNumerator(), fraction.GetDenominator());
+
+            Console.WriteLine("\n\t### Після скорочення дробів ###");
+            index = 0;
+            foreach (Fraction fraction in fractions)
+            {
+                Console.WriteLine($"fractions[{index}] = {fraction}");
+                index++;
+            }
+
+            Console.WriteLine("\n\tУнарні: +, – (для типу Fraction):");
+            fractions.Add(new Fraction());
+            fractions[9] = -fractions[7];
+            Console.WriteLine($"fractions[9] = -({fractions[7]}) = {fractions[9]}");
+
+            fractions.Add(new Fraction());
+            fractions[10] = +fractions[7];
+            Console.WriteLine($"fractions[10] = +({fractions[7]}) = {fractions[10]}");
+
+            Console.WriteLine("\n\tБінарні +, –, *, / (для типу Fraction, Fraction):");
+            fractions.Add(new Fraction());
+            fractions[11] = fractions[1] + fractions[5];
+            Console.WriteLine($"fractions[11] = {fractions[1]} + {fractions[5]} = {fractions[11]}");
+
+            fractions.Add(new Fraction());
+            fractions[12] = fractions[1] - fractions[5];
+            Console.WriteLine($"fractions[12] = {fractions[1]} - {fractions[5]} = {fractions[12]}");
+
+            fractions[1] = new Fraction("1/2");
+            fractions[5] = new Fraction("1/2");
+            fractions[12] = fractions[1] + fractions[5];
+            fractions[12].FractionReduction(fractions[12].GetNumerator(), fractions[12].GetDenominator());
+            Console.WriteLine($"fractions[12] = {fractions[1]} + {fractions[5]} = {fractions[12]}");
+
+            fractions[12] = fractions[1] - fractions[5];
+            fractions[12].FractionReduction(fractions[12].GetNumerator(), fractions[12].GetDenominator());
+            Console.WriteLine($"fractions[12] = {fractions[1]} - {fractions[5]} = {fractions[12]}");
+
+            fractions[1] = new Fraction(0, 3);
+            fractions[5] = new Fraction(0, 8);
+            fractions[12] = fractions[1] - fractions[5];
+            fractions[12].FractionReduction(fractions[12].GetNumerator(), fractions[12].GetDenominator());
+            Console.WriteLine($"fractions[12] = {fractions[1]} - {fractions[5]} = {fractions[12]}");
+
+            fractions[1] = new Fraction(0, 8);
+            fractions[5] = new Fraction(5, 3);
+            fractions[12] = fractions[1] - fractions[5];
+            fractions[12].FractionReduction(fractions[12].GetNumerator(), fractions[12].GetDenominator());
+            Console.WriteLine($"fractions[12] = {fractions[1]} - {fractions[5]} = {fractions[12]}");
+
+            Fraction frac = fractions[5] * fractions[6];
+            Console.WriteLine($"frac = {fractions[5]} * {fractions[6]} = {frac}");
+
+            frac = fractions[5] / fractions[6];
+            Console.WriteLine($"frac = {fractions[5]} / {fractions[6]} = {frac}");
+
+            Console.WriteLine("\n\tБінарні +, –, *, / (для типу Fraction, long):");
+            Console.Write($"fractions[12] = {fractions[12]} + 10 = ");
+            fractions[12] = fractions[12] + 10;
+            fractions[12].FractionReduction(fractions[12].GetNumerator(), fractions[12].GetDenominator());
+            Console.WriteLine($"{fractions[12]}");
+
+            Console.Write($"fractions[12] = {fractions[12]} + -10 = ");
+            fractions[12] = fractions[12] + -10;
+            fractions[12].FractionReduction(fractions[12].GetNumerator(), fractions[12].GetDenominator());
+            Console.WriteLine($"{fractions[12]}");
+
+            Console.Write($"fractions[12] = {10} + {fractions[12]} = ");
+            fractions[12] = 10 + fractions[12];
+            fractions[12].FractionReduction(fractions[12].GetNumerator(), fractions[12].GetDenominator());
+            Console.WriteLine($"{fractions[12]}");
+
+            fractions[7].SetDenominator(3);
+            Console.Write($"fractions[7] = {fractions[7]} - 10 = ");
+            fractions[7] = fractions[7] - 10;
+            Console.Write($"{fractions[7]}  =>  ");
+            fractions[7].FractionReduction(fractions[7].GetNumerator(), fractions[7].GetDenominator());
+            Console.WriteLine($"{fractions[7]}");
+
+            Console.Write($"fractions[7] = 10 - {fractions[7]} = ");
+            fractions[7] = 10 - fractions[7];
+            fractions[7].FractionReduction(fractions[7].GetNumerator(), fractions[7].GetDenominator());
+            Console.WriteLine($"{fractions[7]}");
+
+            frac = fractions[5] * 12;
+            Console.WriteLine($"frac = {fractions[5]} * 12 = {frac}");
+
+            frac = 5 * fractions[5];
+            Console.WriteLine($"frac = 5 * {fractions[5]} = {frac}");
+
+            frac = fractions[5] / 15;
+            Console.WriteLine($"frac = {fractions[5]} / 15 = {frac}");
+
+            frac = 4 / fractions[6];
+            Console.WriteLine($"frac = 4 / {fractions[6]} = {frac}");
+
+            Console.WriteLine("\n\tБінарні порівняння: >, >=, <, <=, ==, !=:");
+            Console.WriteLine($"{fractions[0]} > {fractions[1]}   -->   {fractions[0] > fractions[1]}");
+            Console.WriteLine($"{fractions[1]} < {fractions[2]}   -->   {fractions[1] < fractions[2]}");
+            Console.WriteLine($"{fractions[2]} >= {fractions[3]}   -->   {fractions[2] >= fractions[3]}");
+            Console.WriteLine($"{fractions[3]} <= {fractions[4]}   -->   {fractions[3] <= fractions[4]}");
+            Console.WriteLine($"{fractions[4]} == {fractions[5]}   -->   {fractions[4] == fractions[5]}");
+            Console.WriteLine($"{fractions[5]} != {fractions[6]}   -->   {fractions[5] != fractions[6]}");
+            Console.WriteLine($"{fractions[6]} > 5   -->   {fractions[6] > 5}");
+            Console.WriteLine($"2 < {fractions[6]}   -->   {2 < fractions[6]}");
+            Console.WriteLine($"{fractions[6]} >= 7.74   -->   {fractions[6] >= 7.74}");
+            Console.WriteLine($"8.26 <= {fractions[6]}   -->   {8.26 <= fractions[6]}");
+            fractions[6] = new Fraction("1/2");
+            Console.WriteLine($"{fractions[6]} == 0.5   -->   {fractions[6] == 0.5}");
+            Console.WriteLine($"{fractions[6]} != 0.5   -->   {fractions[6] != 0.5}");
+            Console.WriteLine($"0.5 != {fractions[6]}   -->   {0.5 != fractions[6]}");
+            Console.WriteLine($"0.5 == {fractions[6]}   -->   {0.5 == fractions[6]}");
+            
+            fractions[6] = new Fraction(16, 7);
+            Console.WriteLine("\n\tЯвне перетворення типу Fraction до double:");
+            index = 0;
+            foreach (Fraction fraction in fractions)
+            {
+                Console.WriteLine($"fractions[{index}] = {fraction} = (double)fractions[{index}] = {(double)fraction, 0:f4}");
+                index++;
+            }
+
+            Console.WriteLine("\n\tБінарні +, –, *, / (для типу Fraction, double):");
+            Console.WriteLine($"{frac} + 0.27 = {frac + 0.27}");
+            Console.WriteLine($"31.5 + {frac} = {31.5 + frac}");
+            Console.WriteLine($"{frac} + (-7.31) = {frac + -7.31}");
+            double x = frac + -7.31;
+            Console.WriteLine($"{frac} - {x} = {frac - x}");
+            x = frac - x;
+            Console.WriteLine($"13.55 * {frac} = {13.55 * frac}");
+            Console.WriteLine($"{frac} / {x} = {frac / x}");
+
+            Console.WriteLine("\n\tDoubleToFraction:");
+            frac = frac.DoubleToFraction(-3.58);
+            Console.WriteLine($"frac = -3.58 = {frac}");
+            frac = frac.DoubleToFraction(0.23);
+            Console.WriteLine($"frac = 0.23 = {frac}");
+            frac = frac.DoubleToFraction(71.5834);
+            Console.WriteLine($"frac = 71.5834 = {frac}");
+
+            x = frac - 25.37;
+            Console.WriteLine($"{frac} - 25.37 = {frac - 25.37} = {frac.DoubleToFraction(x)}\n\n");
+        }
+    }
+}
